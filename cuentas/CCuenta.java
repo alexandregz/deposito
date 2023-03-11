a2476430acf044c072d757f9c450c7b3723931f6
Alexandre Espinosa Menor <aemenor@gmail.com>
2023-03-11 02:48:13 +0100

    subida docu de javadoc

--- cuentas/CCuenta.java
+++ cuentas/CCuenta.java
@@ -1,48 +1,108 @@
 package cuentas;
 public class CCuenta {
 
+	/**
+	 * Nome do cliente
+	 */
+	private String nombre;
+	
+	/**
+	 * IBAN da conta
+	 */
+    private String cuenta;
+    
+    /**
+     * saldo da conta
+     */
+    private double saldo;
+    
+    /**
+     * Tipo de interese aplicable
+     */
+    private double tipoInterés;
+	
+	
 
+	/**
+	 * getter nombre
+	 * @return
+	 */
     public String getNombre() {
 		return nombre;
 	}
 
+    /**
+     * setter nombre
+     * @param nombre
+     */
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
 
+	/**
+	 * getter cuenta
+	 * @return
+	 */
 	public String getCuenta() {
 		return cuenta;
 	}
 
+	/**
+	 * setter cuenta
+	 * @param cuenta
+	 */
 	public void setCuenta(String cuenta) {
 		this.cuenta = cuenta;
 	}
 
+	/**
+	 * getter saldo
+	 * @return
+	 */
 	public double getSaldo() {
 		return saldo;
 	}
 
+	/**
+	 * setter saldo
+	 * @param saldo
+	 */
 	public void setSaldo(double saldo) {
 		this.saldo = saldo;
 	}
 
+	/**
+	 * getter tipoInterés
+	 * @return
+	 */
 	public double getTipoInterés() {
 		return tipoInterés;
 	}
 
+	/**
+	 * setter tipoInterés
+	 * @param tipoInterés
+	 */
 	public void setTipoInterés(double tipoInterés) {
 		this.tipoInterés = tipoInterés;
 	}
 
-	private String nombre;
-    private String cuenta;
-    private double saldo;
-    private double tipoInterés;
 
+
+	/**
+	 * Constructor sen parámetros, por defecto
+	 */
     public CCuenta()
     {
     }
 
+    /**
+     * Constructor con todos os atributos. Nota: tipo pasase pero co código actual non se popula.
+     * @param nom
+     * @param cue
+     * @param sal
+     * @param tipo
+     */
     public CCuenta(String nom, String cue, double sal, double tipo)
     {
         nombre =nom;
@@ -50,11 +110,21 @@ public class CCuenta {
         saldo=sal;
     }
 
+    /**
+     * Devolve o saldo da conta
+     * 
+     * @return saldo
+     */
     public double estado()
     {
         return saldo;
     }
 
+    /**
+     * Ingreso en conta. Lanza Exception se "cantidad" é negativa.
+     * @param cantidad
+     * @throws Exception
+     */
     public void ingresar(double cantidad) throws Exception
     {
         if (cantidad<0)
@@ -62,6 +132,11 @@ public class CCuenta {
         saldo = saldo + cantidad;
     }
 
+    /**
+     * Retirada de saldo da conta. Lanza Exception se "cantidad" é negativa.
+     * @param cantidad
+     * @throws Exception
+     */
     public void retirar(double cantidad) throws Exception
     {
         if (cantidad <= 0)
